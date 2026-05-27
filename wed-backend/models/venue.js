069633612bb801cef45e49c04e1becd94f265d78
alka <alkakumari16723@gmail.com>
2026-05-27 11:42:38 +0530

    code updated 27

--- wed-backend/models/venue.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const venueSchema = new mongoose.Schema({
-  name: String,
-  location: String,
-});
-
-module.exports = mongoose.model("Venue", venueSchema);
